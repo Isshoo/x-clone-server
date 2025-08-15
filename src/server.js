f2ef11e65740d8862420f716856b0f0d8f75b6f2
Tods <algy25ng@gmail.com>
2025-08-16 03:39:15 +0800

    added vercel config

--- src/server.js
+++ src/server.js
@@ -38,12 +38,18 @@ app.use((err, req, res, next) => {
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-      console.log(`Server is running on port ${ENV.PORT}`);
-    });
+
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log(`Server is running on port ${ENV.PORT}`);
+      });
+    }
   } catch (error) {
     console.error("Error starting server:", error);
     process.exit(1);
   }
 };
 startServer();
+
+// Export for deployment
+export default app;
